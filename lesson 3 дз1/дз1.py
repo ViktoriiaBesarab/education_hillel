fa4dd5e30d43727cb7d5bae7086c4d166e36fc2c
ViktoriiaBesarab <vikabesarab1997@gmail.com>
2026-01-25 14:15:56 +0200

    lesson 3, home work 1

---
+++ lesson 3 дз1/дз1.py
@@ -0,0 +1,16 @@
+number_1 = int(input("enter a number: "))
+number_2 = int(input("enter another number: "))
+action = input("enter action: ")
+
+if action == "+":
+    print(number_1 + number_2)
+elif action == "-":
+    print(number_1 - number_2)
+elif action == "*":
+    print(number_1 * number_2)
+
+if number_2 != "0":
+    print(number_1 / number_2)
+else:
+    print("Error")
+
