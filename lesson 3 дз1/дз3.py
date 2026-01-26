f6077012edab3ffe07540be4f22cec07cc3ad326
ViktoriiaBesarab <vikabesarab1997@gmail.com>
2026-01-26 16:48:24 +0200

    lesson 3, home work 3

--- lesson 3 дз1/дз3.py
+++ lesson 3 дз1/дз3.py
@@ -3,35 +3,11 @@ my_list_2 = [1, 2, 3, 4, 5]
 my_list_3 = [1, ]
 my_list_4 = []
 
-if len(my_list_1) == 0:
-    result =[[], []]
-else:
-    mid = (len(my_list_1) + 1) // 2
-    result = [my_list_1[:mid], my_list_1[mid:]]
+def my_list(my_list):
+    mid = (len(my_list) + 1) //2
+    return [my_list[:mid], my_list[mid:]]
 
-print(result)
-
-if len(my_list_2) == 0:
-    result =[[], []]
-else:
-    mid = (len(my_list_2) + 1) // 2
-    result = [my_list_2[:mid], my_list_2[mid:]]
-
-print(result)
-
-if len(my_list_3) == 0:
-    result =[[], []]
-else:
-    mid = (len(my_list_3) + 1) // 2
-    result = [my_list_3[:mid], my_list_3[mid:]]
-
-print(result)
-
-if len(my_list_4) == 0:
-    result =[[], []]
-else:
-    mid = (len(my_list_4) + 1) // 2
-    result = [my_list_4[:mid], my_list_4[mid:]]
-
-print(result)
-
+print(my_list(my_list_1))
+print(my_list(my_list_2))
+print(my_list(my_list_3))
+print(my_list(my_list_4))
