b9bd7d2caec8371b10c7d2bd389f81d8a446e699
ViktoriiaBesarab <vikabesarab1997@gmail.com>
2026-01-30 12:16:11 +0200

    home work 2, lesson 4

--- lesson 4/дз lesson 4.2.py
+++ lesson 4/дз lesson 4.2.py
@@ -2,11 +2,13 @@ my_list = [0, 1, 7, 2, 4, 8]
 #my_list = [6]
 #my_list = []
 
-sum = 0
-for index, number in enumerate(my_list):
-    if index % 2 == 0:
-        sum += number
-
-last = my_list[-1]
-result = sum * last
-print(result)
+if my_list:
+    sum = 0
+    for index, number in enumerate(my_list):
+        if index % 2 == 0:
+         sum += number
+    last = my_list[-1]
+    result = sum * last
+    print(result)
+else:
+    print("Empty list")
