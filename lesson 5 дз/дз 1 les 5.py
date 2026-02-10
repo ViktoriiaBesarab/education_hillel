cd3267e3e6e9ace42114211cd556621fa3caf1e7
ViktoriiaBesarab <vikabesarab1997@gmail.com>
2026-02-10 16:43:57 +0200

    home work 1. lesson 5

--- lesson 5 дз/дз 1 les 5.py
+++ lesson 5 дз/дз 1 les 5.py
@@ -3,16 +3,19 @@ import keyword
 
 name = input("Enter name: ")
 can_use = True
+punct = string.punctuation.replace("_", " ")
 
 if name[0].isdigit():
     can_use = False
-elif name.islower():
+elif any(a.isupper() for a in name):
     can_use = False
 elif " " in name:
     can_use = False
+elif name.count("_") > 1:
+    can_use = False
 elif name in keyword.kwlist:
     can_use = False
-elif any(h in string.punctuation for h in name):
+elif any(h in punct for h in name):
     can_use = False
 
 print(can_use)
