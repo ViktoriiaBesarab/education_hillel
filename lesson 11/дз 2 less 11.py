1e9cb496e833f05a38c884cd47593f983cc71d8b
ViktoriiaBesarab <vikabesarab1997@gmail.com>
2026-02-25 11:46:07 +0200

    home work 2. lesson 11

---
+++ lesson 11/дз 2 less 11.py
@@ -0,0 +1,18 @@
+def generate_cube_numbers(end):
+    number = 2
+    for number in range(2, end):
+        cube = number ** 3
+        if cube >= end:
+          return
+        yield cube
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+isgenerator(gen)
+result_1 = list(generate_cube_numbers(10))
+result_2 = list(generate_cube_numbers(100))
+result_3 = list(generate_cube_numbers(1000))
+print(result_1)
+print(result_2)
+print(result_3)
