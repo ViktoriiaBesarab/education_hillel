8d98fe72e083bb37a225cce9e959ea247e06a354
ViktoriiaBesarab <vikabesarab1997@gmail.com>
2026-03-06 18:36:17 +0200

    home work 1. lesson 13

---
+++ lesson 13/дз 1 lesson 13.py
@@ -0,0 +1,67 @@
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return (f"Gender: {self.gender}, Age: {self.age}, First: {self.first_name}, Last: {self.last_name}")
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return (f"Student: {self.gender}, {self.age} {self.first_name}, {self.last_name}, {self.record_book}")
+
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+        print(f"Student {student.last_name} added to group {self.number}")
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+            print(f"Student {last_name} removed from group {self.number}")
+        else:
+            print(f"Student {last_name} not in group {self.number}")
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = ''
+        for student in self.group:
+            all_students += str(student) + '\n'
+        return f'Number: {self.number}\n{all_students}'
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+
+print(gr)
+
+str(gr.find_student('Jobs'))
+gr.find_student('Jobs2')
+isinstance(gr.find_student('Jobs'), Student)
+
+gr.delete_student('Taylor')
+print(gr)
+
+gr.delete_student('Taylor')
