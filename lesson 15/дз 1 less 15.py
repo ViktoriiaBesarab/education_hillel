eef770491570ae2b25b727f693891819be2047c5
ViktoriiaBesarab <vikabesarab1997@gmail.com>
2026-03-09 18:01:38 +0200

    home work 1. lesson 15

---
+++ lesson 15/дз 1 less 15.py
@@ -0,0 +1,34 @@
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square(self):
+        return self.width * self.height
+
+    def __eq__(self, other):
+        return self.width == other.width and self.height == other.height
+
+    def __add__(self, other):
+        return Rectangle(self.width + other.width, self.height + other.height)
+
+    def __mul__(self, n):
+        return Rectangle(self.width * n, self.height * n)
+
+    def __str__(self):
+        return f'{self.width} x {self.height}'
+
+
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+assert r1.get_square()
+assert r2.get_square()
+
+r3 = r1 + r2
+assert r3.get_square()
+
+r4 = r1 * 4
+assert r4.get_square()
+
+assert r2 == Rectangle(3, 6)
