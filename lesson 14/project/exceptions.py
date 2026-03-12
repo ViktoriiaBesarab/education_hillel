94233c216b7c2f244f571a4971425f744e080029
ViktoriiaBesarab <vikabesarab1997@gmail.com>
2026-03-12 08:16:32 +0200

    home work 2. lesson 14

---
+++ lesson 14/project/exceptions.py
@@ -0,0 +1,2 @@
+class GroupOverflowException(Exception):
+    pass
---
+++ lesson 14/project/group.py
@@ -0,0 +1,34 @@
+from exceptions import GroupOverflowException
+
+
+class Group:
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= 10:
+            raise GroupOverflowException("Group cannot contain more than 10 students")
+
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+        else:
+            print(f"Student {last_name} not found")
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = ""
+        for student in self.group:
+            all_students += str(student) + "\n"
+
+        return f"Number: {self.number}\n{all_students}"
